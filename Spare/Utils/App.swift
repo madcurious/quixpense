a1aacb9623af20c32d10296c6a33e4491c5aa2f9
Matt Quiros <mattquiros@gmail.com>
2016-04-20 17:54:04 +0800

    Added app state manager and core data stack

---
+++ Spare/Utils/App.swift
@@ -0,0 +1,38 @@
+//
+//  App.swift
+//  Spare
+//
+//  Created by Matt Quiros on 20/04/2016.
+//  Copyright © 2016 Matt Quiros. All rights reserved.
+//
+
+import UIKit
+import BNRCoreDataStack
+
+private let kSharedInstance = App()
+
+class App {
+    
+    class var instance: App {
+        return kSharedInstance
+    }
+    
+    var coreDataStack: CoreDataStack!
+    
+    private init() {
+        // Initialise CoreData stack.
+        CoreDataStack.constructSQLiteStack(withModelName: "Spare") {[unowned self] (result) in
+            switch result {
+            case .Success(let stack):
+                self.coreDataStack = stack
+                
+            case .Failure(let error as NSError):
+                fatalError(error.description)
+                
+            default:
+                fatalError()
+            }
+        }
+    }
+    
+}
