6942f2a84dcc737a2058548f61de1bdb19d81c69
Matt Quiros <mattquiros@gmail.com>
2016-04-25 21:45:36 +0800

    Added color map to category editor

--- Spare/Screens/EditCategory/EditCategoryVC.swift
+++ Spare/Screens/EditCategory/EditCategoryVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Color_Picker_for_iOS
 
 class EditCategoryVC: BaseVC {
     
@@ -15,11 +16,21 @@ class EditCategoryVC: BaseVC {
     var category: Category
     
     let customView = __ECVCView.instantiateFromNib() as __ECVCView
+    let colorMap: HRColorMapView
     
     init(category: Category?) {
         let context = App.state.coreDataStack.newBackgroundWorkerMOC()
         self.managedObjectContext = context
-        self.category = category ?? Category(managedObjectContext: context)
+        
+        
+        if let category = category,
+            let colorHex = category.color as? Int {
+            self.category = category
+            self.colorMap = HRColorMapView.defaultColorMap(UIColor(hex: colorHex))
+        } else {
+            self.category = Category(managedObjectContext: context)
+            self.colorMap = HRColorMapView.defaultColorMap(nil)
+        }
         
         super.init()
     }
@@ -30,6 +41,24 @@ class EditCategoryVC: BaseVC {
     
     override func loadView() {
         self.view = self.customView
+        self.customView.colorMapContainer.addSubviewAndFill(self.colorMap)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.colorMap.addTarget(self, action: #selector(handleTapOnColorMap(_:)), forControlEvents: .ValueChanged)
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapOnView)))
+    }
+    
+    func handleTapOnView() {
+        self.dismissKeyboard()
+    }
+    
+    func handleTapOnColorMap(sender: AnyObject) {
+        // When the color map is tapped, a color has been selected and the cursor
+        // should be displayed.
+        self.colorMap.cursor.hidden = false
     }
     
 }
---
+++ Spare/Screens/EditCategory/HRColorMapView.swift
@@ -0,0 +1,37 @@
+//
+//  HRColorMapView.swift
+//  Spare
+//
+//  Created by Matt Quiros on 25/04/2016.
+//  Copyright © 2016 Matt Quiros. All rights reserved.
+//
+
+import UIKit
+import Color_Picker_for_iOS
+
+extension HRColorMapView {
+    
+    var cursor: HRColorCursor {
+        for subview in self.subviews {
+            if let cursor = subview as? HRColorCursor {
+                return cursor
+            }
+        }
+        fatalError("Did not find a cursor subview.")
+    }
+    
+    class func defaultColorMap(selectedColor: UIColor?) -> HRColorMapView {
+        let colorMap = HRColorMapView.colorMapWithFrame(CGRectZero, saturationUpperLimit: 1)
+        colorMap.tileSize = 1
+        colorMap.brightness = 1
+        
+        if let color = selectedColor {
+            colorMap.color = color
+        } else {
+            colorMap.cursor.hidden = true
+        }
+        
+        return colorMap
+    }
+    
+}
