e15e75eefa70b589ac40019a7bb2fe5e44ac6695
Matt Quiros <mattquiros@gmail.com>
2017-02-17 03:36:44 +0800

    Added other button boxes and icons

--- Spare/Screens/ExpenseForm/main form/_EFVCView.swift
+++ Spare/Screens/ExpenseForm/main form/_EFVCView.swift
@@ -15,19 +15,38 @@ class _EFVCView: UIView {
     
     let amountBox = _EFVCAmountBox.instantiateFromNib()
     let dateBox = _EFVCButtonBox.instantiateFromNib()
+    let categoryBox = _EFVCButtonBox.instantiateFromNib()
+    let subcategoriesBox = _EFVCButtonBox.instantiateFromNib()
+    let paymentMethodsBox = _EFVCButtonBox.instantiateFromNib()
+    let itemNameBox = _EFVCButtonBox.instantiateFromNib()
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.scrollView.alwaysBounceVertical = true
+        self.stackView.spacing = 6
         
-        self.dateBox.fieldLabel.text = "DATE"
-        self.dateBox.iconImageView.image = UIImage.templateNamed("dateIcon")
-        self.dateBox.fieldButton.placeholder = "Required"
+        let buttonBoxData = [
+            ("DATE", "dateIcon", "Required"),
+            ("CATEGORY", "categoryIcon", "Food, Transportation, etc."),
+            ("SUBCATEGORIES", "subcategoriesIcon", "Personal, Work, etc."),
+            ("PAYMENT METHODS", "paymentMethodsIcon", "Cash, Credit, Rewards Card, etc."),
+            ("ITEM NAME", "itemNameIcon", "e.g. Breakfast at Tiffany's")
+        ]
         
-        let fieldBoxes = [self.amountBox, self.dateBox]
-        for box in fieldBoxes {
-            self.stackView.addArrangedSubview(box)
+        let fieldBoxes = [self.amountBox, self.dateBox, self.categoryBox, self.subcategoriesBox, self.paymentMethodsBox, self.itemNameBox]
+        for i in 0 ..< fieldBoxes.count {
+            switch i {
+            case 1 ... 5:
+                let data = buttonBoxData[i - 1]
+                fieldBoxes[i].fieldLabel.text = data.0
+                fieldBoxes[i].iconImageView.image = UIImage.templateNamed(data.1)
+                (fieldBoxes[i] as! _EFVCButtonBox).fieldButton.placeholder = data.2
+                fallthrough
+                
+            default:
+                self.stackView.addArrangedSubview(fieldBoxes[i])
+            }
         }
     }
     
