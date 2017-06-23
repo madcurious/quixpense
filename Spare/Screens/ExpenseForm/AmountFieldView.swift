ef3b360aa87c5d913c9886080c777fa0060aed6b
Matt Quiros <mattquiros@Matts-MacBook-Pro.local>
2017-06-23 15:56:34 +0800

    Added amount field left vie

--- Spare/Screens/ExpenseForm/AmountFieldView.swift
+++ Spare/Screens/ExpenseForm/AmountFieldView.swift
@@ -12,23 +12,38 @@ class AmountFieldView: UIView, Themeable {
     
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var textField: UITextField!
+    
+    let currencyLabel = UILabel()
     let invalidCharacterSet = CharacterSet.decimalNumberCharacterSet().inverted
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.applyTheme()
         
+        self.currencyLabel.text = AmountFormatter.currencySymbol()
+        self.currencyLabel.textAlignment = .left
+        
         self.textField.keyboardType = .decimalPad
+        self.textField.leftView = self.currencyLabel
+        self.textField.leftViewMode = .always
         self.textField.delegate = self
     }
     
     func applyTheme() {
+        self.currencyLabel.textColor = Global.theme.color(for: .placeholder)
+        self.currencyLabel.font = Global.theme.font(for: .regularText)
+        
         self.textField.font = Global.theme.font(for: .regularText)
         self.textField.textColor = Global.theme.color(for: .regularText)
         self.textField.attributedPlaceholder = NSAttributedString(
             string: "0.00",
             font: Global.theme.font(for: .regularText),
             textColor: Global.theme.color(for: .placeholder))
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.currencyLabel.frame = CGRect(x: 0, y: 0, width: 20, height: self.currencyLabel.intrinsicContentSize.height)
     }
     
 }
