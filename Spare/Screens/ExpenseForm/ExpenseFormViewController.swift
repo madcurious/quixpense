dcf372ed0343e14c27e554fe5bdbb3dc4587b00a
Matt Quiros <mattquiros@gmail.com>
2017-07-17 14:53:41 +0800

    Added base UI for simplified date picker

--- Spare/Screens/ExpenseForm/ExpenseFormViewController.swift
+++ Spare/Screens/ExpenseForm/ExpenseFormViewController.swift
@@ -11,13 +11,13 @@ import UIKit
 class ExpenseFormViewController: UIViewController {
     
     let customView = ExpenseFormView.instantiateFromNib()
-    let suggestionList = SuggestionsViewController()
-    let suggestionListHeight: CGFloat = {
-        if UIScreen.main.nativeSize.height == 568 {
-            return CGFloat(44 * 2)
-        }
-        return CGFloat(44 * 3)
-    }()
+//    let suggestionList = SuggestionsViewController()
+//    let suggestionListHeight: CGFloat = {
+//        if UIScreen.main.nativeSize.height == 568 {
+//            return CGFloat(44 * 2)
+//        }
+//        return CGFloat(44 * 3)
+//    }()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -39,33 +39,33 @@ class ExpenseFormViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = BarButtonItems.make(
             .done, target: self, action: #selector(handleTapOnDoneButton))
         
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self,
-                                       selector: #selector(handleKeyboardWillShow(_:)),
-                                       name: Notification.Name.UIKeyboardWillShow,
-                                       object: nil)
-        notificationCenter.addObserver(self,
-                                       selector: #selector(handleKeyboardWillHide(_:)),
-                                       name: Notification.Name.UIKeyboardWillHide,
-                                       object: nil)
-        
-        let textFieldsWithSuggestions = [self.customView.categoryFieldView.textField, self.customView.tagFieldView.textField]
-        for textField in textFieldsWithSuggestions {
-            notificationCenter.addObserver(self,
-                                           selector: #selector(handleTextFieldTextDidChange),
-                                           name: Notification.Name.UITextFieldTextDidChange,
-                                           object: textField)
-            notificationCenter.addObserver(self,
-                                           selector: #selector(handleTextFieldDidEndEditing(_:)),
-                                           name: Notification.Name.UITextFieldTextDidEndEditing,
-                                           object: textField)
-        }
+//        let notificationCenter = NotificationCenter.default
+//        notificationCenter.addObserver(self,
+//                                       selector: #selector(handleKeyboardWillShow(_:)),
+//                                       name: Notification.Name.UIKeyboardWillShow,
+//                                       object: nil)
+//        notificationCenter.addObserver(self,
+//                                       selector: #selector(handleKeyboardWillHide(_:)),
+//                                       name: Notification.Name.UIKeyboardWillHide,
+//                                       object: nil)
+//
+//        let textFieldsWithSuggestions = [self.customView.categoryFieldView.textField, self.customView.tagFieldView.textField]
+//        for textField in textFieldsWithSuggestions {
+//            notificationCenter.addObserver(self,
+//                                           selector: #selector(handleTextFieldTextDidChange),
+//                                           name: Notification.Name.UITextFieldTextDidChange,
+//                                           object: textField)
+//            notificationCenter.addObserver(self,
+//                                           selector: #selector(handleTextFieldDidEndEditing(_:)),
+//                                           name: Notification.Name.UITextFieldTextDidEndEditing,
+//                                           object: textField)
+//        }
         
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapOnView))
         tapGestureRecognizer.cancelsTouchesInView = false
         self.customView.addGestureRecognizer(tapGestureRecognizer)
         
-        self.customView.scrollView.delegate = self
+//        self.customView.scrollView.delegate = self
         
         self.customView.categoryFieldView.textField.delegate = self
         self.customView.tagFieldView.textField.delegate = self
@@ -111,91 +111,91 @@ extension ExpenseFormViewController {
         
     }
     
-    @objc func handleKeyboardWillShow(_ notification: Notification) {
-        guard let userInfo = notification.userInfo,
-            let keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue as? CGRect,
-            let animationDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSValue as? NSNumber as? Double
-            else {
-                return
-        }
+//    @objc func handleKeyboardWillShow(_ notification: Notification) {
+//        guard let userInfo = notification.userInfo,
+//            let keyboardFrame = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue as? CGRect,
+//            let animationDuration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSValue as? NSNumber as? Double
+//            else {
+//                return
+//        }
+//
+//        var contentOffsetY = CGFloat(0)
+//        let minimumSuggestionListY = UIScreen.main.nativeSize.height - keyboardFrame.height - self.suggestionListHeight
+//        var suggestionListY = minimumSuggestionListY
+//
+//        if self.customView.categoryFieldView.textField.isFirstResponder {
+//            let lowerLeftCorner: CGPoint = {
+//                var corner = self.customView.categoryFieldView.frame.origin
+//                corner.y += self.customView.categoryFieldView.bounds.size.height
+//                corner = UIApplication.shared.keyWindow!.convert(corner, from: self.customView.categoryFieldView)
+//                return corner
+//            }()
+//
+//            if lowerLeftCorner.y > minimumSuggestionListY {
+//                // The field appears below the suggestion list so take the deficit into account in the offset.
+//                contentOffsetY += lowerLeftCorner.y - suggestionListY
+//            } else {
+//                // Adjust the suggestion list.
+//                suggestionListY = lowerLeftCorner.y
+//            }
+//
+//            self.suggestionList.fetchSuggestions(for: self.customView.categoryFieldView.textField.text, classifierType: .category)
+//            self.embedChildViewController(self.suggestionList, toView: self.view, fillSuperview: false)
+//            self.suggestionList.view.frame = {
+//                var frame = CGRect(x: 20,
+//                                   y: suggestionListY,
+//                                   width: self.view.bounds.size.width - 40,
+//                                   height: self.suggestionListHeight)
+//                frame = self.view.convert(frame, from: UIApplication.shared.keyWindow!)
+//                return frame
+//            }()
+//            self.suggestionList.view.setNeedsLayout()
+//            self.suggestionList.view.layoutIfNeeded()
+//            self.suggestionList.view.isHidden = true
+//        }
+//
+//        // DEBUG
+//        else {
+//            self.suggestionList.unembedFromParentViewController()
+//        }
+//
+//        let newInsets = UIEdgeInsetsMake(0, 0, keyboardFrame.size.height - self.tabBarController!.tabBar.bounds.size.height, 0)
+//        self.customView.scrollView.contentInset = newInsets
+//        self.customView.scrollView.scrollIndicatorInsets = newInsets
+//
+//        UIView.animate(
+//            withDuration: animationDuration,
+//            animations: {[unowned self] in
+//                self.customView.scrollView.contentOffset = CGPoint(x: 0, y: contentOffsetY)
+//            }, completion: {[unowned self] _ in
+//                self.suggestionList.view.isHidden = false
+//        })
+//    }
         
-        var contentOffsetY = CGFloat(0)
-        let minimumSuggestionListY = UIScreen.main.nativeSize.height - keyboardFrame.height - self.suggestionListHeight
-        var suggestionListY = minimumSuggestionListY
-        
-        if self.customView.categoryFieldView.textField.isFirstResponder {
-            let lowerLeftCorner: CGPoint = {
-                var corner = self.customView.categoryFieldView.frame.origin
-                corner.y += self.customView.categoryFieldView.bounds.size.height
-                corner = UIApplication.shared.keyWindow!.convert(corner, from: self.customView.categoryFieldView)
-                return corner
-            }()
-            
-            if lowerLeftCorner.y > minimumSuggestionListY {
-                // The field appears below the suggestion list so take the deficit into account in the offset.
-                contentOffsetY += lowerLeftCorner.y - suggestionListY
-            } else {
-                // Adjust the suggestion list.
-                suggestionListY = lowerLeftCorner.y
-            }
-            
-            self.suggestionList.fetchSuggestions(for: self.customView.categoryFieldView.textField.text, classifierType: .category)
-            self.embedChildViewController(self.suggestionList, toView: self.view, fillSuperview: false)
-            self.suggestionList.view.frame = {
-                var frame = CGRect(x: 20,
-                                   y: suggestionListY,
-                                   width: self.view.bounds.size.width - 40,
-                                   height: self.suggestionListHeight)
-                frame = self.view.convert(frame, from: UIApplication.shared.keyWindow!)
-                return frame
-            }()
-            self.suggestionList.view.setNeedsLayout()
-            self.suggestionList.view.layoutIfNeeded()
-            self.suggestionList.view.isHidden = true
-        }
-        
-        // DEBUG
-        else {
-            self.suggestionList.unembedFromParentViewController()
-        }
-        
-        let newInsets = UIEdgeInsetsMake(0, 0, keyboardFrame.size.height - self.tabBarController!.tabBar.bounds.size.height, 0)
-        self.customView.scrollView.contentInset = newInsets
-        self.customView.scrollView.scrollIndicatorInsets = newInsets
-        
-        UIView.animate(
-            withDuration: animationDuration,
-            animations: {[unowned self] in
-                self.customView.scrollView.contentOffset = CGPoint(x: 0, y: contentOffsetY)
-            }, completion: {[unowned self] _ in
-                self.suggestionList.view.isHidden = false
-        })
-    }
-    
-    @objc func handleKeyboardWillHide(_ notification: Notification) {
-        self.customView.scrollView.contentInset = .zero
-        self.customView.scrollView.scrollIndicatorInsets = .zero
-        
-        self.suggestionList.unembedFromParentViewController()
-        self.suggestionList.view.frame = .zero
-    }
-    
-    @objc func handleTextFieldTextDidChange(_ notification: Notification) {
-        guard let textField = notification.object as? UITextField
-            else {
-                return
-        }
-        
-        let query = textField.text
-        
-        if textField == self.customView.categoryFieldView.textField {
-            self.suggestionList.fetchSuggestions(for: query, classifierType: .category)
-        }
-    }
-    
-    @objc func handleTextFieldDidEndEditing(_ notification: Notification) {
-        
-    }
+//    @objc func handleKeyboardWillHide(_ notification: Notification) {
+//        self.customView.scrollView.contentInset = .zero
+//        self.customView.scrollView.scrollIndicatorInsets = .zero
+//
+//        self.suggestionList.unembedFromParentViewController()
+//        self.suggestionList.view.frame = .zero
+//    }
+//
+//    @objc func handleTextFieldTextDidChange(_ notification: Notification) {
+//        guard let textField = notification.object as? UITextField
+//            else {
+//                return
+//        }
+//
+//        let query = textField.text
+//
+//        if textField == self.customView.categoryFieldView.textField {
+//            self.suggestionList.fetchSuggestions(for: query, classifierType: .category)
+//        }
+//    }
+//
+//    @objc func handleTextFieldDidEndEditing(_ notification: Notification) {
+//
+//    }
     
 }
 
@@ -213,15 +213,15 @@ extension ExpenseFormViewController: CategoryPickerViewControllerDelegate {
     
 }
 
-// MARK: - UIScrollViewDelegate
-
-extension ExpenseFormViewController: UIScrollViewDelegate {
-    
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        self.suggestionList.unembedFromParentViewController()
-    }
-    
-}
+//// MARK: - UIScrollViewDelegate
+//
+//extension ExpenseFormViewController: UIScrollViewDelegate {
+//    
+//    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+//        self.suggestionList.unembedFromParentViewController()
+//    }
+//    
+//}
 
 // MARK: - UITextFieldDelegate
 
---
+++ Spare/Screens/ExpenseForm/old/DateFieldView.swift
@@ -0,0 +1,216 @@
+//
+//  DateFieldView.swift
+//  Spare
+//
+//  Created by Matt Quiros on 23/06/2017.
+//  Copyright © 2017 Matt Quiros. All rights reserved.
+//
+
+import UIKit
+
+class DateFieldView: UIView, Themeable {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var fieldStackView: UIStackView!
+    
+    @IBOutlet weak var dayContainer: UIView!
+    @IBOutlet weak var dayTextField: DateTextField!
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    @IBOutlet weak var monthContainer: UIView!
+    @IBOutlet weak var monthTextField: DateTextField!
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    @IBOutlet weak var yearContainer: UIView!
+    @IBOutlet weak var yearTextField: DateTextField!
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    @IBOutlet var textFields: [DateTextField]!
+    @IBOutlet var textFieldLabels: [UILabel]!
+    @IBOutlet var slashLabels: [UILabel]!
+    
+    let invalidCharacterSet = CharacterSet.wholeNumberCharacterSet().inverted
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.applyTheme()
+        
+        self.imageView.image = UIImage.templateNamed("dateIcon")
+        
+        self.dayLabel.text = "DD"
+        self.monthLabel.text = "MM"
+        self.yearLabel.text = "YYYY"
+        for slashLabel in self.slashLabels {
+            slashLabel.text = "/"
+        }
+        
+        for textField in self.textFields {
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(handleTextDidChangeNotification(_:)),
+                name: Notification.Name.UITextFieldTextDidChange,
+                object: textField)
+            textField.delegate = self
+            textField.dateTextFieldDelegate = self
+            textField.keyboardType = .numberPad
+        }
+        
+        self.setToCurrentDate()
+    }
+    
+    func applyTheme() {
+        self.imageView.tintColor = Global.theme.color(for: .fieldIcon)
+        
+        for textField in self.textFields {
+            textField.font = Global.theme.font(for: .regularText)
+            textField.textColor = Global.theme.color(for: .regularText)
+            textField.textAlignment = .center
+        }
+        self.dayTextField.attributedPlaceholder = NSAttributedString(
+            string: "31",
+            font: Global.theme.font(for: .regularText),
+            textColor: Global.theme.color(for: .placeholder))
+        self.monthTextField.attributedPlaceholder = NSAttributedString(
+            string: "12",
+            font: Global.theme.font(for: .regularText),
+            textColor: Global.theme.color(for: .placeholder))
+        self.yearTextField.attributedPlaceholder = NSAttributedString(
+            string: "1990",
+            font: Global.theme.font(for: .regularText),
+            textColor: Global.theme.color(for: .placeholder))
+        
+        for slashLabel in self.slashLabels {
+            slashLabel.font = Global.theme.font(for: .regularText)
+            slashLabel.textColor = Global.theme.color(for: .placeholder)
+        }
+        
+        for textFieldLabel in self.textFieldLabels {
+            textFieldLabel.font = Global.theme.font(for: .dateTextFieldLabel)
+            textFieldLabel.textColor = Global.theme.color(for: .placeholder)
+            textFieldLabel.textAlignment = .center
+        }
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        // Tapping on the year and the month stack views activate their corresponding text fields.
+        // Tapping on anywhere else activates the day text field.
+        let convertedPoint = self.convert(point, to: self.fieldStackView)
+        switch convertedPoint {
+        case _ where self.yearContainer.frame.contains(convertedPoint):
+            return self.yearTextField
+        case _ where self.monthContainer.frame.contains(convertedPoint):
+            return self.monthTextField
+        case _ where self.frame.contains(point):
+            return self.dayTextField
+        default:
+            return super.hitTest(point, with: event)
+        }
+    }
+    
+    @objc func handleTextDidChangeNotification(_ notification: Notification) {
+        guard let textField = notification.object as? UITextField,
+            let text = textField.text
+            else {
+                return
+        }
+        
+        switch textField {
+        case self.yearTextField where text.characters.count == 4:
+            self.monthTextField.becomeFirstResponder()
+            
+        case self.monthTextField where text.characters.count == 2:
+            self.dayTextField.becomeFirstResponder()
+            
+        default:
+            break
+        }
+    }
+    
+    // Convenience function for setting the value of all three text fields based on a date.
+    func setDate(_ date: Date) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy"
+        self.yearTextField.text = formatter.string(from: date)
+        
+        formatter.dateFormat = "MM"
+        self.monthTextField.text = formatter.string(from: date)
+        
+        formatter.dateFormat = "dd"
+        self.dayTextField.text = formatter.string(from: date)
+    }
+    
+    func setToCurrentDate() {
+        self.setDate(Date())
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+}
+
+extension DateFieldView: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard string.rangeOfCharacter(from: self.invalidCharacterSet) == nil
+            else {
+                return false
+        }
+        
+        guard let currentText = textField.text as NSString?
+            else {
+                return true
+        }
+        let resultingText = currentText.replacingCharacters(in: range, with: string)
+        
+        switch textField {
+        case self.yearTextField:
+            if resultingText.characters.count > 4 {
+                return false
+            }
+            
+        default:
+            if resultingText.characters.count > 2 {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+}
+
+extension DateFieldView: DateTextFieldDelegate {
+    
+    func dateTextFieldDidRequestFocusTransfer(_ textField: DateTextField) {
+        switch textField {
+        case self.dayTextField:
+            self.monthTextField.becomeFirstResponder()
+        case self.monthTextField:
+            self.yearTextField.becomeFirstResponder()
+        default:
+            break
+        }
+    }
+    
+}
+
+class DateTextField: UITextField {
+    
+    var dateTextFieldDelegate: DateTextFieldDelegate?
+    
+    override func deleteBackward() {
+        // If the text field was already empty when the backspace was pressed,
+        // request for focus to be transferred to the text field on the left.
+        if let text = self.text,
+            text.isEmpty {
+            self.dateTextFieldDelegate?.dateTextFieldDidRequestFocusTransfer(self)
+        }
+        super.deleteBackward()
+    }
+    
+}
+
+protocol DateTextFieldDelegate {
+    func dateTextFieldDidRequestFocusTransfer(_ textField: DateTextField)
+}
